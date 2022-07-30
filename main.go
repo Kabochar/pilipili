83eac6520567bdd1a468cbba8ada7d17a4fdea4e
kabocha <anemailbox@163.com>
2022-07-30 12:30:33 +0800

    feat: add graceful shutdown (#3)

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"pilipili/util"
+	"syscall"
+	"time"
+
 	"pilipili/conf"
 	"pilipili/server"
 )
@@ -11,5 +19,40 @@ func main() {
 
 	// 装载路由
 	r := server.NewRouter()
-	r.Run(":9000")
+	bind := os.Getenv("API_BIND")
+
+	srv := &http.Server{
+		Addr:    bind,
+		Handler: r,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			util.Log().Panic("listen: %s\n", err)
+		}
+	}()
+
+	// graceful shutdown implement，copied from gin office docs
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscanll.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	util.Log().Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		util.Log().Panic("Server Shutdown:", err)
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		util.Log().Println("timeout of 2 seconds.")
+	}
+	util.Log().Println("Server exiting")
 }
